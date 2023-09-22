c8130bd944c7094b3da937794d47e1a40ca8034d
Abdul Rehman <abdulrehmang571@gmail.com>
2023-09-22 06:45:45 +0500

    wrote builtTree function

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ class Node {
     this.right = null;
   }
 }
+
+function builtTree(arr, start = 0, end = arr.length - 1) {
+  const mid = Math.ceil((start + end) / 2);
+  if (start > end) return null;
+  const node = new Node(arr[mid]);
+  node.left = builtTree(arr, start, mid - 1);
+  node.right = builtTree(arr, mid + 1, end);
+  return node;
+}
+console.log(builtTree([...new Set([1, 2, 3, 3, 4, 4])]));
