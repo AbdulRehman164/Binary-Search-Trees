e931858febbc9c49bf2bfe59e6e7620ea9ed2555
Abdul Rehman <abdulrehmang571@gmail.com>
2023-09-22 07:34:27 +0500

    called the builtTree in the root in Tree class and copied prettyprint method in the tree class

--- index.js
+++ index.js
@@ -14,6 +14,29 @@ function builtTree(arr, start = 0, end = arr.length - 1) {
   node.right = builtTree(arr, mid + 1, end);
   return node;
 }
-console.log(
-  builtTree([...new Set([4, 4, 6, 10, 2, 6, 5, 3, 60].sort((a, b) => a - b))])
-);
+
+class Tree {
+  constructor(arr) {
+    this.root = builtTree([...new Set(arr.sort((a, b) => a - b))]);
+  }
+
+  prettyPrint(node = this.root, prefix = '', isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? '│   ' : '    '}`,
+        false
+      );
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+  }
+}
+
+const tree = new Tree([1, 23, 5, 56, 7, 34, 23, 6, 7, 5]);
+tree.prettyPrint();
